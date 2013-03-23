57c2d3f2c8421b409b69515bf7d4305c58fd9aea
John Grafton <john.grafton@gmail.com>
2013-03-23 10:21:27 -0700

    added permissions to profile files

--- modules/profile/manifests/config.pp
+++ modules/profile/manifests/config.pp
@@ -13,18 +13,24 @@ class profile::config {
   file {"${home_dir}/.bash_profile":
     ensure  => present,
     require => User[$user],
+    mode    => 0644,
+    owner   => jgrafton,
     source  => "puppet:///modules/profile/bash_profile",
   }
 
   file {"${home_dir}/.screenrc":
     ensure  => present,
     require => User[$user],
+    mode    => 0644,
+    owner   => jgrafton,
     source  => "puppet:///modules/profile/screenrc",
   }
 
   file {"${home_dir}/.vimrc":
     ensure  => present,
     require => User[$user],
+    mode    => 0644,
+    owner   => jgrafton,
     source  => "puppet:///modules/profile/vimrc",
   }
 
