6752b5f042aaf3f2fea79047f1756a64e6da783b
John Grafton <john.grafton@gmail.com>
2013-03-23 10:36:12 -0700

    added OS specific bash profiles

--- modules/profile/manifests/config.pp
+++ modules/profile/manifests/config.pp
@@ -2,12 +2,30 @@ class profile::config {
   $user='jgrafton'
 
   case $::operatingsystem {
-    Darwin: { $home_dir="/Users/${user}" }
-    Solaris: { $home_dir="/home/${user}" }
-    Debian: { $home_dir="/home/${user}" }
-    Debian: { $home_dir="/home/${user}" }
-    CentOS: { $home_dir="/home/${user}" }
-    Default: { $home_dir="/home/${user}" }
+    Darwin: { 
+      $home_dir="/Users/${user}"
+      $bash_profile="puppet:///modules/profile/bash_profile_osx"
+    }
+    Solaris: { 
+      $home_dir="/home/${user}" 
+      $bash_profile="puppet:///modules/profile/bash_profile_solaris"
+    }
+    Debian: { 
+      $home_dir="/home/${user}" 
+      $bash_profile="puppet:///modules/profile/bash_profile_linux"
+    }
+    Debian: { 
+      $home_dir="/home/${user}" 
+      $bash_profile="puppet:///modules/profile/bash_profile_linux"
+    }
+    CentOS: { 
+      $home_dir="/home/${user}" 
+      $bash_profile="puppet:///modules/profile/bash_profile_linux"
+    }
+    Default: { 
+      $home_dir="/home/${user}" 
+      $bash_profile="puppet:///modules/profile/bash_profile_linux"
+    }
   }
 
   file {"${home_dir}/.bash_profile":
@@ -15,7 +33,7 @@ class profile::config {
     require => User[$user],
     mode    => 0644,
     owner   => jgrafton,
-    source  => "puppet:///modules/profile/bash_profile",
+    source  => $bash_profile,
   }
 
   file {"${home_dir}/.screenrc":
