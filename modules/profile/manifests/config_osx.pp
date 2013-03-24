2d4aa87315cf861b78eb39523d39a9c7b22895f5
John Grafton <john.grafton@gmail.com>
2013-03-23 19:17:01 -0700

    corrected home directory's on OSX

--- modules/profile/manifests/config_osx.pp
+++ modules/profile/manifests/config_osx.pp
@@ -1,21 +1,21 @@
 class profile::config_osx {
   $user='jgrafton'
 
-  file {"/Users/${home}/.bash_profile":
+  file {"/Users/${user}/.bash_profile":
     ensure  => present,
     mode    => 0644,
     owner   => jgrafton,
     source  => 'puppet:///modules/profile/bash_profile_osx'
   }
 
-  file {"${home_dir}/.screenrc":
+  file {"/Users/${user}/.screenrc":
     ensure  => present,
     mode    => 0644,
     owner   => jgrafton,
     source  => "puppet:///modules/profile/screenrc",
   }
 
-  file {"${home_dir}/.vimrc":
+  file {"/Users/${user}/.vimrc":
     ensure  => present,
     mode    => 0644,
     owner   => jgrafton,
